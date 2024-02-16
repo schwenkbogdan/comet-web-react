d36198a3f0cb5d7624944ab07a203267d17e228f
Debian virtual <no name>
2024-02-16 22:42:12 +0700

    initial

---
+++ registration-page/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import RegistrationForm from './RegistrationForm';
+import ThemeProviderWrapper from './ThemeProvider';
+
+function App() {
+  return (
+
+      <div>
+        <RegistrationForm />
+      </div>
+
+  );
+}
+
+export default App;
